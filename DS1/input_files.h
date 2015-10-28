0f1cd67115a8e127dcfeb9d3ecbc19a8e75dbfa2
Jan Kaspar <jan.kaspar@gmail.com>
2015-10-28 19:49:37 +0100

    fixed source directory of ntuple files

--- DS1/input_files.h
+++ DS1/input_files.h
@@ -15,7 +15,8 @@ void InitInputFiles()
 
 	// 2015_10_13_fill4489, DS1
 
-	std::string prefix = "root://eostotem.cern.ch//eos/totem/user/j/jkaspar/reco/sr,pass2/";
+	std::string prefix = "root://eostotem.cern.ch//eos/totem/data/cmstotem/2015/90m/Totem/Ntuple/version1/";
+
 	input_files.push_back(prefix + "9836_EVB11_1.0-9_ntuple.root");
 	input_files.push_back(prefix + "9838_EVB11_1.0-9_ntuple.root");
 	input_files.push_back(prefix + "9840_EVB11_1.0-9_ntuple.root");
